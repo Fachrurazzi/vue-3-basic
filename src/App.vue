93bf2d3b66356bf8d7559563cf8e5cd4180ae549
Fachrurazzi <razzytirta@gmail.com>
2022-02-01 16:41:06 +0800

    Mixins

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,17 @@
 <template>
-  <div class="tab">
-    <button 
-      v-for="tab in tabs" 
-      :key="tab" 
-      :class="['tab-button', { active: currentTab === tab }]" 
-      @click="currentTab = tab"
-    >{{ tab }}</button>
-   </div>
-    <keep-alive>
-      <component :is="currentTabComponent" />
-    </keep-alive>
+  <pre>{{ $data }}</pre>
+  <button @click="onPress">Click Me</button>
 </template>
 
 <script>
-import Home from './components/Home.vue'
-import Post from './components/Post.vue'
-import About from './components/About.vue'
+import basicMixins from './mixins/basic'
+import mergeMixins from './mixins/merge'
 export default {
-  components: { Home, Post, About },
-  data() {
+  mixins: [ basicMixins, mergeMixins ],
+  data(){
     return {
-      currentTab: "Home",
-      tabs: ["Home", "Post", "About"]
-    }
-  },
-  computed: {
-    currentTabComponent() {
-      return this.currentTab.toLowerCase()
+      name: 'Fachrurazzi'
     }
   }
 }
 </script>
-
-<style>
-.tab {
-  font-family: sans-serif;
-  border: 1px solid #eee;
-  border-radius: 2px;
-  padding: 20px 30px;
-  margin-top: 1cm;
-  margin-bottom: 40px;
-  user-select: none;
-  overflow-x: auto;
-}
-.tab-button {
-  padding: 6px 10px;
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-  border: 1px solid #ccc;
-  cursor: pointer;
-  background: #f0f0f0;
-  margin-bottom: -px;
-  margin-right: -1px;
-}
-.tab-button:hover{
-  background: #e0e0e0;
-}
-.tab-buton.active {
-  background: #e0e0e0;
-}
-.demo-tab {
-  border: 1px solid #ccc;
-  padding: 10px;
-}
-</style>
