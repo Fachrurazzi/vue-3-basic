c2ba786b97b42d5fe04de1b50de79a336233d83e
Fachrurazzi <razzytirta@gmail.com>
2022-02-01 09:18:26 +0800

    declarative rendering

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+
+  <span :title="message">Balalalal</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hi World'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
