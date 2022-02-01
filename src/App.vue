b397bcfe7d204f027acd2932a7e8f8c7125f564f
Fachrurazzi <razzytirta@gmail.com>
2022-02-01 15:20:34 +0800

    Global Component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,10 @@
 <template>
-<child>
-  <template v-slot:default="{ data }">
-    <span>Judul : {{ data }}</span>
-  </template>
-
-    <template v-slot:content="{ data }">
-    <span>Konten : {{ data }}</span>
-  </template>
-
-    <template v-slot:footer="{ data }">
-    <span>Footer : {{ data }} </span>
-  </template>
-</child>
-
+  <child-component/>
 </template>
 
 <script>
-import Child from "./components/ChildComponent.vue"
 export default {
-  components: {Child},
-  data() {
-    return {
-      message: ''
-    }
-  }
+  
 }
 </script>
 
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,20 +1,9 @@
 <template>
-    <div>Title</div>
-    <slot :data="title"/>
-    <div>Content</div>
-    <slot name="content" :data="content"/>
-    <div>Footer</div>
-    <slot name="footer" :data="footer"/>
+    <div>Learn</div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            title:'Belajar Vue 3',
-            content: 'Belajar Framework Vue 3',
-            footer: 'copyright 2022'
-        }
-    }
+
 }
 </script>
