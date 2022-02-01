3316fe935887e4069e754b6a3587b043275550dd
Fachrurazzi <razzytirta@gmail.com>
2022-02-01 10:15:31 +0800

    Lifecycle vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
-  <div>Ini adalah parent component</div>
-  <child-component 
-    v-for="name in names" 
-    :text="name"
-  />
+<div id="lifecycle">{{ message }}</div>
+
+<button 
+  @click="message= 'tulisan setelah dirender ulang'"
+  >Update
+</button>
 </template>
 
 <script>
-import ChildComponent from './components/ChildComponent.vue'
 export default {
-  components:{ ChildComponent },
   data() {
     return {
-      names: ['andi', 'anto', 'dono']
+      message: 'first text'
     }
+  },
+  beforeCreate() {
+    console.log('before create');
+  },
+  created() {
+    console.log('created');
+    console.log(this.message);
+    console.log(document.getElementById('lifecycle'));
+  },
+  beforeMount() {
+    console.log('before mount');
+  },
+  mounted() {
+    console.log('mounted');
+    console.log(this.message);
+    console.log(document.getElementById('lifecycle'));
+  },
+  beforeUpdate() {
+    console.log('before update');
+    console.log(document.getElementById('lifecycle').textContent);
+  },
+  updated() {
+    console.log('updated');
+    console.log(document.getElementById('lifecycle').textContent);
+  },
+  beforeUnmount() {
+    console.log('before unmount')
+  },
+  unmounted() {
+    console.log('unmounted');
   }
 }
 </script>
