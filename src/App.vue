7d5fc2882ea09c471096867373af0cc6431daf69
Fachrurazzi <razzytirta@gmail.com>
2022-02-01 09:23:33 +0800

    methods

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
-    {{ message }}
-  </div>
+  <div>{{ count }}</div>
 
-  <span :title="message">Balalalal</span>
+  <button @click="count++">Add 1</button>
+  <button @click="addCount">Add Count</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'Hi World'
+      count: 0
+    }
+  },
+  methods: {
+    addCount() {
+      this.count += 2;
     }
   }
+
 }
 </script>
 
