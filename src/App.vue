7e159909619cd269e38935ad3c7a92e20f0d989d
Fachrurazzi <razzytirta@gmail.com>
2022-02-01 10:19:16 +0800

    v-once

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,15 @@
 <template>
-<div id="lifecycle">{{ message }}</div>
+  <div v-once>{{ message }}</div>
 
-<button 
-  @click="message= 'tulisan setelah dirender ulang'"
-  >Update
-</button>
+  <button @click="message='hahahhihih'">Update</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'first text'
+      message: 'Hi Guys'
     }
-  },
-  beforeCreate() {
-    console.log('before create');
-  },
-  created() {
-    console.log('created');
-    console.log(this.message);
-    console.log(document.getElementById('lifecycle'));
-  },
-  beforeMount() {
-    console.log('before mount');
-  },
-  mounted() {
-    console.log('mounted');
-    console.log(this.message);
-    console.log(document.getElementById('lifecycle'));
-  },
-  beforeUpdate() {
-    console.log('before update');
-    console.log(document.getElementById('lifecycle').textContent);
-  },
-  updated() {
-    console.log('updated');
-    console.log(document.getElementById('lifecycle').textContent);
-  },
-  beforeUnmount() {
-    console.log('before unmount')
-  },
-  unmounted() {
-    console.log('unmounted');
   }
 }
 </script>
